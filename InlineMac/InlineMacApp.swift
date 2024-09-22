85b630e4e83577c6b144a5e564df47f49fe51c9d
Mohamad <mo.rajbi@gmail.com>
2024-09-22 13:19:22 +0330

    Add Inline macOS app

---
+++ InlineMac/InlineMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  InlineMacApp.swift
+//  InlineMac
+//
+//  Created by Mohammad Rajabifard on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct InlineMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InlineMacTests/InlineMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  InlineMacTests.swift
+//  InlineMacTests
+//
+//  Created by Mohammad Rajabifard on 9/22/24.
+//
+
+import Testing
+@testable import InlineMac
+
+struct InlineMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
