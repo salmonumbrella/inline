88af2267c0b1046e87fda81a7fed19f7584d5668
Dena Sohrabi <denasohrabi62@gmail.com>
2024-10-01 16:35:26 +0330

    Add enter name page

---
+++ InlineIOS/Components/AddAccount.swift
@@ -0,0 +1,51 @@
+import HeadlineKit
+import SwiftUI
+
+struct AddAccount: View {
+    @State private var name = ""
+    @FocusState private var isFocused: Bool
+
+    private var placeHolder: String = "Dena"
+
+    @EnvironmentObject var nav: Navigation
+    @EnvironmentObject var api: ApiClient
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 0) {
+            Image(systemName: "person.fill")
+                .font(.largeTitle)
+                .padding(.top, 50)
+                .foregroundColor(.cyan)
+            Spacer()
+            Text("Enter your name")
+                .font(Font.custom("Red Hat Display", size: 28))
+                .fontWeight(.medium)
+
+            TextField(placeHolder, text: $name)
+                .textFieldStyle(OutlinedTextFieldStyle(isFocused: isFocused))
+                .focused($isFocused)
+                .keyboardType(.emailAddress)
+                .textInputAutocapitalization(.never)
+                .autocorrectionDisabled(true)
+                .frame(minWidth: 220)
+                .fixedSize()
+            Spacer()
+        }
+        .padding(.horizontal, 44)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .safeAreaInset(edge: .bottom) {
+            Button {
+           
+            } label: {
+                Text("Continue")
+            }
+            .buttonStyle(SimpleButtonStyle())
+            .padding(.bottom, 18)
+            .padding(.horizontal, 44)
+        }
+    }
+}
+
+#Preview {
+    AddAccount()
+}
