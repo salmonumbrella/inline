38f28b91204431cbb169dbd2b017676b8cd76b43
Dena Sohrabi <denasohrabi62@gmail.com>
2024-10-03 13:22:06 +0330

    remove extra logs

--- HeadlineKit/Sources/HeadlineKit/CurrentData.swift
+++ HeadlineKit/Sources/HeadlineKit/CurrentData.swift
@@ -7,10 +7,7 @@ public final class CurrentDataModel: ObservableObject, @unchecked Sendable {
     @Published public var token: String? = nil
 
     public func saveToken(_ token: String) {
-        print("💶 TOKEN before saving\(self.token)")
         self.token = token
-        print("💶 TOKEN \(token)")
-        print("💶 TOKEN Self \(self.token)")
     }
 
     public init() {}
