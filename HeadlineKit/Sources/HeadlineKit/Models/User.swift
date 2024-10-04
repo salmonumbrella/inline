c9d8f4a619c8e47ee306ce78d63a3d9a87d6dc95
Dena Sohrabi <denasohrabi62@gmail.com>
2024-10-04 18:12:49 +0330

    Add user model

---
+++ HeadlineKit/Sources/HeadlineKit/Models/User.swift
@@ -0,0 +1,38 @@
+import Foundation
+import GRDB
+
+public struct User: FetchableRecord, Identifiable, Codable, Hashable, PersistableRecord {
+    public var id: Int64
+    public var email: String
+    public var firstName: String
+    public var lastName: String?
+    public var createdAt: Date?
+
+    public init(email: String, firstName: String, lastName: String? = nil) {
+        id = Int64.random(in: 1 ... 5000)
+        self.email = email
+        self.firstName = firstName
+        self.lastName = lastName
+        createdAt = Date.now
+    }
+}
+
+public extension User {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case email
+        case firstName
+        case lastName
+        case createdAt
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decode(Int64.self, forKey: .id)
+        email = try container.decode(String.self, forKey: .email)
+        firstName = try container.decode(String.self, forKey: .firstName)
+        lastName = try container.decodeIfPresent(String.self, forKey: .lastName)
+        createdAt = try container.decodeIfPresent(Date.self, forKey: .createdAt) ?? .now
+    }
+}
