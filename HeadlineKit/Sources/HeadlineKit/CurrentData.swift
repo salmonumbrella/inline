d696a5369ed95d93797afd6e02e82a936bd9435c
Dena Sohrabi <denasohrabi62@gmail.com>
2024-10-04 18:12:41 +0330

    Update Current Data

--- HeadlineKit/Sources/HeadlineKit/CurrentData.swift
+++ HeadlineKit/Sources/HeadlineKit/CurrentData.swift
@@ -5,24 +5,26 @@ import SwiftUI
 
 public final class Auth: @unchecked Sendable {
     public static let shared = Auth()
-
-    public var token: String?
-
+    private var cachedToken: String?
     private let keychain = KeychainSwift()
-    private var groupKey: String {
-        #if os(macOS)
-        return "2487AN8AL4.chat.inline"
-        #elseif os(iOS)
-        return "group.chat.inline"
-        #endif
+    private var groupKey: String = "2487AN8AL4.keychainGroup"
+
+    public func saveToken(_ token: String?) {
+        cachedToken = token
+
+        /// In logout, the token will be nil, and the keychain does not get nil data, so it keeps the token. The cachedToken is always a token after the first login, and by adding "if let" here, we just keep it always full (not nil). If the token was nil, we should delete it so that cachedToken is empty and we can return to the root path because we return cachedToken in the getToken function.
+        if let token = token {
+            keychain.set(token, forKey: groupKey)
+        } else {
+            keychain.delete(groupKey)
+        }
     }
 
-    public func saveToken(_ token: String) {
-        self.token = token
-        keychain.set(token, forKey: groupKey)
+    public func getToken() -> String? {
+        return cachedToken
     }
 
-    public init() {
-        token = keychain.get(groupKey)
+    private init() {
+        cachedToken = keychain.get(groupKey)
     }
 }
