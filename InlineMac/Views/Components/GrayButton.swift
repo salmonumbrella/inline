c87cba2c1e988e0b9b7c49b743c5dc85deaf3efb
Mohamad <mo.rajbi@gmail.com>
2024-10-21 20:04:56 +0330

    Hover

--- InlineMac/Views/Components/GrayButton.swift
+++ InlineMac/Views/Components/GrayButton.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct GrayButton<Label>: View
-where Label: View
+    where Label: View
 {
     var action: () -> Void
     var label: Label
@@ -12,13 +12,17 @@ where Label: View
     }
     
     @FocusState private var isFocused: Bool
+    @State private var hovered: Bool = false
     
     var body: some View {
         if #available(macOS 14.0, *) {
             Button(action: action, label: { label })
-                .buttonStyle(GrayButtonStyle(isFocused: isFocused))
+                .buttonStyle(GrayButtonStyle(isFocused: isFocused, hovered: hovered))
                 .focusEffectDisabled(true)
                 .focused($isFocused)
+                .onHover {
+                    hovered = $0
+                }
         } else {
             Button(action: action, label: { label })
                 .buttonStyle(GrayButtonStyle())
@@ -27,27 +31,36 @@ where Label: View
     
     struct GrayButtonStyle: ButtonStyle {
         var isFocused: Bool
+        var hovered: Bool
         
-        init(isFocused: Bool) {
+        init(isFocused: Bool, hovered: Bool) {
             self.isFocused = isFocused
+            self.hovered = hovered
         }
         
         init() {
             self.isFocused = false
+            self.hovered = false
         }
         
         func makeBody(configuration: Configuration) -> some View {
             let background: Color = configuration.isPressed ?
-                .primary.opacity(0.1) :
-                .primary.opacity(0.06)
+                .primary.opacity(0.15) : hovered ?
+                .primary.opacity(0.13) :
+                .primary.opacity(0.09)
             let scale: CGFloat = configuration.isPressed ? 0.95 : 1
+            let textOpacity: Double = configuration.isPressed ? 0.8 : 0.95
+            let animation =
+                (isFocused ? 100 : 0) +
+                (configuration.isPressed ? 10 : 0) +
+                (hovered ? 1 : 0)
             
-            configuration.label
-                .font(.system(size: 14, weight: .regular))
-                .frame(height: 36)
+            return configuration.label
+                .font(.system(size: 15, weight: .regular))
+                .frame(height: 38)
                 .padding(.horizontal)
                 .background(background)
-                .foregroundStyle(.primary)
+                .foregroundStyle(.primary.opacity(textOpacity))
                 .cornerRadius(10)
                 .overlay(content: {
                     if isFocused {
@@ -57,19 +70,19 @@ where Label: View
                     }
                 })
                 .scaleEffect(x: scale, y: scale)
-                .animation(.snappy, value: configuration.isPressed)
+                .animation(.snappy.speed(2.0), value: animation)
         }
     }
 }
 
 #Preview("Gray Button (Light)") {
-    GrayButton(action: { }, label: { Text("Continue") })
+    GrayButton(action: {}, label: { Text("Continue") })
         .padding()
         .preferredColorScheme(.light)
 }
 
 #Preview("Gray Button (Dark)") {
-    GrayButton(action: { }, label: { Text("Continue") })
+    GrayButton(action: {}, label: { Text("Continue") })
         .padding()
         .preferredColorScheme(.dark)
 }
