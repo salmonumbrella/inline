75a01729706ce99891c95a47a279e3e68fd3c60c
Dena Sohrabi <denasohrabi62@gmail.com>
2024-11-27 17:55:23 +0330

    - [x] Fix dm item UI

--- InlineIOS/Views/Main/ChatRowView.swift
+++ InlineIOS/Views/Main/ChatRowView.swift
@@ -9,21 +9,41 @@ struct ChatRowView: View {
     item.chat?.type ?? .privateChat
   }
 
+  private func formatDate(_ date: Date) -> String {
+    let calendar = Calendar.current
+    if calendar.isDateInToday(date) {
+      let formatter = DateFormatter()
+      formatter.dateFormat = "h:mm a"
+      formatter.amSymbol = ""  // Remove AM
+      return formatter.string(from: date).replacingOccurrences(of: " PM", with: "PM")
+    } else {
+      let formatter = DateFormatter()
+      formatter.dateFormat = "MMM d, h:mm"
+      return formatter.string(from: date)
+    }
+  }
+
   var body: some View {
     HStack {
-      UserAvatar(user: item.user, size: 28)
+      UserAvatar(user: item.user, size: 36)
         .padding(.trailing, 6)
       VStack(alignment: .leading) {
-        Text(type == .privateChat ? item.user.firstName ?? "" : item.chat?.title ?? "")
-          .fontWeight(.medium)
+        HStack {
+          Text(type == .privateChat ? item.user.firstName ?? "" : item.chat?.title ?? "")
+            .fontWeight(.medium)
+          Spacer()
+          Text(formatDate(item.message?.date ?? Date()))
+            .font(.callout)
+            .foregroundColor(.secondary)
+
+        }
         Text(item.message?.text ?? "")
-          .font(.body)
+          .font(.callout)
           .foregroundColor(.secondary)
       }
     }
     .frame(height: 48)
     .frame(maxWidth: .infinity, alignment: .leading)
     .contentShape(Rectangle())
-
   }
 }
