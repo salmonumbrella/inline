88fe0ac7ed7af4954d0d147b2170eb691c67f7d0
Dena Sohrabi <denasohrabi62@gmail.com>
2024-11-27 17:07:00 +0330

    Clean up code

--- InlineIOS/Views/Onboarding/Code.swift
+++ InlineIOS/Views/Onboarding/Code.swift
@@ -4,19 +4,21 @@ import SwiftUI
 
 struct Code: View {
   var email: String
-  @State private var code = ""
-  @State var animate: Bool = false
-  @FocusState private var isFocused: Bool
-  @State var errorMsg: String = ""
-  @FormState var formState
-  @State private var isInputValid: Bool = false
-
-  private var placeHolder: String = "xxxxxx"
+  var placeHolder: String = "xxxxxx"
   let characterLimit = 6
+
+  @State var code = ""
+  @State var animate: Bool = false
+  @State var errorMsg: String = ""
+  @State var isInputValid: Bool = false
+
+  @FocusState private var isFocused: Bool
+  @FormState var formState
 
   @EnvironmentObject var nav: OnboardingNavigation
   @EnvironmentObject var api: ApiClient
   @EnvironmentObject var userData: UserData
+
   @Environment(\.appDatabase) var database
   @Environment(\.auth) private var auth
 
@@ -27,75 +29,23 @@ struct Code: View {
   var body: some View {
     VStack(alignment: .leading, spacing: 4) {
       AnimatedLabel(animate: $animate, text: "Enter the code")
-      TextField(placeHolder, text: $code)
-        .focused($isFocused)
-        .keyboardType(.numberPad)
-        .textInputAutocapitalization(.never)
-        .monospaced()
-        .kerning(5)
-        .autocorrectionDisabled(true)
-        .font(.title2)
-        .fontWeight(.semibold)
-
-        .padding(.vertical, 8)
-        .onSubmit {
-          submitCode()
-        }
-        .onChange(of: isFocused) { _, newValue in
-          withAnimation(.smooth(duration: 0.15)) {
-            animate = newValue
-          }
-        }
-        .onChange(of: isInputValid) {
-          DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            withAnimation {
-              isInputValid = true
-            }
-          }
-        }
-        .onChange(of: code) { _, newValue in
-          if newValue.count > characterLimit {
-            code = String(newValue.prefix(characterLimit))
-          }
-          validateInput()
-          if newValue.count == characterLimit {
-            submitCode()
-          }
-        }
-      Text(errorMsg)
-        .font(.callout)
-        .foregroundColor(.red)
+      codeInput
+      hint
     }
     .padding(.horizontal, OnboardingUtils.shared.hPadding)
     .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
     .safeAreaInset(edge: .bottom) {
-      VStack(alignment: .center) {
-        HStack(spacing: 2) {
-          Text("Code sent to \(email).")
-            .font(.callout)
-            .foregroundColor(.secondary)
-          Button("Edit") {
-            nav.pop()
-          }
-          .font(.callout)
-        }
-
-        Button(formState.isLoading ? "Verifying..." : "Continue") {
-          submitCode()
-        }
-        .buttonStyle(SimpleButtonStyle())
-        .frame(maxWidth: .infinity)
-        .padding(.horizontal, OnboardingUtils.shared.hPadding)
-        .padding(.bottom, OnboardingUtils.shared.buttonBottomPadding)
-        .disabled(!isInputValid || formState.isLoading)
-        .opacity((!isInputValid || formState.isLoading) ? 0.5 : 1)
-      }
+      bottomArea
     }
     .onAppear {
       isFocused = true
     }
   }
 
+}
+
+// MARK: - Helper Methods
+extension Code {
   private func validateInput() {
     errorMsg = ""
     isInputValid = code.count == characterLimit
@@ -147,6 +97,80 @@ struct Code: View {
         formState.reset()
         isInputValid = false
       }
+    }
+  }
+}
+
+// MARK: - Views
+extension Code {
+  @ViewBuilder
+  var codeInput: some View {
+    TextField(placeHolder, text: $code)
+      .focused($isFocused)
+      .keyboardType(.numberPad)
+      .textInputAutocapitalization(.never)
+      .monospaced()
+      .kerning(5)
+      .autocorrectionDisabled(true)
+      .font(.title2)
+      .fontWeight(.semibold)
+
+      .padding(.vertical, 8)
+      .onSubmit {
+        submitCode()
+      }
+      .onChange(of: isFocused) { _, newValue in
+        withAnimation(.smooth(duration: 0.15)) {
+          animate = newValue
+        }
+      }
+      .onChange(of: isInputValid) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+          withAnimation {
+            isInputValid = true
+          }
+        }
+      }
+      .onChange(of: code) { _, newValue in
+        if newValue.count > characterLimit {
+          code = String(newValue.prefix(characterLimit))
+        }
+        validateInput()
+        if newValue.count == characterLimit {
+          submitCode()
+        }
+      }
+  }
+
+  @ViewBuilder
+  var hint: some View {
+    Text(errorMsg)
+      .font(.callout)
+      .foregroundColor(.red)
+  }
+
+  @ViewBuilder
+  var bottomArea: some View {
+    VStack(alignment: .center) {
+      HStack(spacing: 2) {
+        Text("Code sent to \(email).")
+          .font(.callout)
+          .foregroundColor(.secondary)
+        Button("Edit") {
+          nav.pop()
+        }
+        .font(.callout)
+      }
+
+      Button(formState.isLoading ? "Verifying..." : "Continue") {
+        submitCode()
+      }
+      .buttonStyle(SimpleButtonStyle())
+      .frame(maxWidth: .infinity)
+      .padding(.horizontal, OnboardingUtils.shared.hPadding)
+      .padding(.bottom, OnboardingUtils.shared.buttonBottomPadding)
+      .disabled(!isInputValid || formState.isLoading)
+      .opacity((!isInputValid || formState.isLoading) ? 0.5 : 1)
     }
   }
 }
