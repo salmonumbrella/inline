177c14c589d245a3acc477da50896dcc3aec41e4
Mohamad <mo.rajbi@gmail.com>
2024-12-08 23:50:02 +0330

    Add ignore safe area to user avatar

--- InlineUI/Sources/InlineUI/UserAvatar.swift
+++ InlineUI/Sources/InlineUI/UserAvatar.swift
@@ -15,29 +15,43 @@ public struct UserAvatar: View, Equatable {
   let email: String?
   let username: String?
   let size: CGFloat
+  let ignoresSafeArea: Bool
 
-  public init(user: User, size: CGFloat = 32) {
+  public init(user: User, size: CGFloat = 32, ignoresSafeArea: Bool = false) {
     self.firstName = user.firstName
     self.lastName = user.lastName
     self.email = user.email
     self.username = user.username
     self.size = size
+    self.ignoresSafeArea = ignoresSafeArea
   }
 
-  public init(apiUser: ApiUser, size: CGFloat = 32) {
+  public init(apiUser: ApiUser, size: CGFloat = 32, ignoresSafeArea: Bool = false) {
     self.firstName = apiUser.firstName
     self.lastName = apiUser.lastName
     self.email = apiUser.email
     self.username = apiUser.username
     self.size = size
+    self.ignoresSafeArea = ignoresSafeArea
   }
 
-  public var body: some View {
+  @ViewBuilder
+  public var avatar: some View {
     InitialsCircle(
       firstName: firstName ?? email?.components(separatedBy: "@").first ?? "User",
       lastName: lastName,
       size: size
     )
     .frame(width: size, height: size)
+  }
+
+  public var body: some View {
+    if ignoresSafeArea {
+      avatar
+        // Important so the toolbar safe area doesn't affect it
+        .ignoresSafeArea()
+    } else {
+      avatar
+    }
   }
 }
