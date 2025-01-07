a1137642f8ebeb9a6ff160707b51c8e05464f4e4
Mohamad <mo.rajbi@gmail.com>
2025-01-07 18:12:28 +0330

    Improve attributed string cache

--- InlineMac/Views/Chat/Message/CacheAttrs.swift
+++ InlineMac/Views/Chat/Message/CacheAttrs.swift
@@ -1,4 +1,5 @@
 import AppKit
+import InlineKit
 
 class CacheAttrs {
   static var shared = CacheAttrs()
@@ -12,6 +13,20 @@ class CacheAttrs {
   
   func get(key: String) -> NSAttributedString? {
     cache.object(forKey: NSString(string: key))
+  }
+  
+  func getKey(_ message: Message) -> String {
+    "\(message.text ?? "")___\(message.stableId)"
+  }
+  
+  func get(message: Message) -> NSAttributedString? {
+    // consider a hash here. // note: need to add ID otherwise messages with same text will be overriding each other styles
+    let key = getKey(message)
+    return cache.object(forKey: NSString(string: key))
+  }
+  
+  func set(message: Message, value: NSAttributedString) {
+    cache.setObject(value, forKey: NSString(string: getKey(message)))
   }
   
   func set(key: String, value: NSAttributedString) {
