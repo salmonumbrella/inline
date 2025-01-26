ecb221c29757e46a45e7bba767bbf0a94eb17170
Mohamad <mo.rajbi@gmail.com>
2025-01-26 18:46:13 +0330

    Add saved message to chat icon

--- apple/InlineMac/Views/Chat/ChatIcon.swift
+++ apple/InlineMac/Views/Chat/ChatIcon.swift
@@ -6,21 +6,33 @@ struct ChatIcon: View {
   enum PeerType {
     case chat(Chat)
     case user(User)
+    case savedMessage(User)
   }
 
   var peer: PeerType
-
   var size: CGFloat = 34
+
   var body: some View {
     switch peer {
-      case .chat:
-        Image(systemName: "bubble.middle.bottom.fill")
-          .resizable()
-          .scaledToFit()
-          .frame(width: size, height: size)
+      case let .chat(thread):
+        InitialsCircle(name: thread.title ?? "", size: size, symbol: "bubble.fill")
+
+      // raw icon
+//        HStack {
+//          Image(systemName: "bubble.fill")
+//            .resizable()
+//            .scaledToFit()
+//            .frame(width: size - 6.0, height: size - 6.0)
+//            .fixedSize()
+//        }
+//        .frame(width: size, height: size)
+//        .fixedSize()
 
       case let .user(user):
         UserAvatar(user: user, size: size)
+
+      case let .savedMessage(user):
+        InitialsCircle(name: user.firstName ?? user.username ?? "", size: size, symbol: "bookmark.fill")
     }
   }
 }
