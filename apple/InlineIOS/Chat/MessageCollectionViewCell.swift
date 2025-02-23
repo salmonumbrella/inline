d7766cf36991e5d5b3959b020b10f2a37f1b7ba4
Dena Sohrabi <denasohrabi62@gmail.com>
2025-02-23 21:33:46 +0330

    Refactor collectionView cell

--- apple/InlineIOS/Chat/MessageCollectionViewCell.swift
+++ apple/InlineIOS/Chat/MessageCollectionViewCell.swift
@@ -5,22 +5,15 @@ import UIKit
 
 class MessageCollectionViewCell: UICollectionViewCell {
   static let reuseIdentifier = "MessageCell"
-  private var messageView: UIMessageView?
-  private var avatarHostingController: UIHostingController<UserAvatar>?
+
+  var messageView: UIMessageView?
+  var avatarHostingController: UIHostingController<UserAvatar>?
+
+  var isThread: Bool = false
+  var outgoing: Bool = false
   var fromOtherSender: Bool = false
-  var messageContent: FullMessage? = nil
-
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    // Set content view priorities to allow proper sizing
-    contentView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-    contentView.setContentHuggingPriority(.defaultLow, for: .horizontal)
-  }
-
-  @available(*, unavailable)
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+  var message: FullMessage!
+  var spaceId: Int64 = 0
 
   lazy var nameLabel: UILabel = {
     var label = UILabel()
@@ -31,22 +24,108 @@ class MessageCollectionViewCell: UICollectionViewCell {
     return label
   }()
 
-  func configure(with message: FullMessage, fromOtherSender: Bool, spaceId: Int64) {
-    messageContent = message
-    var isThread: Bool {
-      message.peerId.isThread
-    }
-    var outgoing: Bool {
-      message.message.out == true
-    }
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupContentSize()
+  }
 
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  func configure(with message: FullMessage, fromOtherSender: Bool, spaceId: Int64) {
+    isThread = message.peerId.isThread
+    outgoing = message.message.out == true
+    self.message = message
+    self.fromOtherSender = fromOtherSender
+    self.spaceId = spaceId
     nameLabel.text = message.from?.firstName ?? "USER"
 
-    messageView?.removeFromSuperview()
-    nameLabel.removeFromSuperview()
-    avatarHostingController?.view.removeFromSuperview()
-    avatarHostingController = nil
+    resetCell()
 
+    setupOutcomingThreadMessage()
+    setupBaseMessageConstraints()
+
+    contentView.transform = CGAffineTransform(scaleX: 1, y: -1)
+  }
+
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    resetCell()
+  }
+
+  override func preferredLayoutAttributesFitting(
+    _ layoutAttributes: UICollectionViewLayoutAttributes
+  ) -> UICollectionViewLayoutAttributes {
+    let attributes = super.preferredLayoutAttributesFitting(layoutAttributes)
+    layoutIfNeeded()
+
+    let targetSize = CGSize(
+      width: layoutAttributes.frame.width,
+      height: UIView.layoutFittingCompressedSize.height
+    )
+
+    let size = contentView.systemLayoutSizeFitting(
+      targetSize,
+      withHorizontalFittingPriority: .required,
+      verticalFittingPriority: .fittingSizeLevel
+    )
+
+    attributes.frame.size = size
+    return attributes
+  }
+}
+
+extension MessageCollectionViewCell {
+  func setupContentSize() {
+    contentView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+    contentView.setContentHuggingPriority(.defaultLow, for: .horizontal)
+  }
+
+  func setupBaseMessageConstraints() {
+    let newMessageView = UIMessageView(fullMessage: message, spaceId: spaceId)
+    newMessageView.translatesAutoresizingMaskIntoConstraints = false
+    contentView.addSubview(newMessageView)
+
+    let topConstraint: NSLayoutConstraint = if isThread, fromOtherSender {
+      newMessageView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 2)
+    } else {
+      newMessageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: fromOtherSender ? 12 : 2)
+    }
+
+    let leadingConstraint: NSLayoutConstraint = if isThread {
+      if outgoing {
+        newMessageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 32)
+      } else if fromOtherSender, let avatarView = avatarHostingController?.view {
+        newMessageView.leadingAnchor.constraint(equalTo: avatarView.trailingAnchor, constant: -2)
+      } else {
+        newMessageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 32)
+      }
+    } else {
+      newMessageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor)
+    }
+
+    let trailingConstraint: NSLayoutConstraint = if isThread {
+      if outgoing, let avatarView = avatarHostingController?.view {
+        newMessageView.trailingAnchor.constraint(equalTo: avatarView.leadingAnchor, constant: 2)
+      } else {
+        newMessageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -32)
+      }
+    } else {
+      newMessageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+    }
+    NSLayoutConstraint.activate([
+      leadingConstraint,
+      trailingConstraint,
+      topConstraint,
+      newMessageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+    ])
+
+    messageView = newMessageView
+  }
+
+  func setupOutcomingThreadMessage() {
     if isThread, fromOtherSender {
       contentView.addSubview(nameLabel)
 
@@ -93,68 +172,12 @@ class MessageCollectionViewCell: UICollectionViewCell {
 
       NSLayoutConstraint.activate(constraints)
     }
-
-    let newMessageView = UIMessageView(fullMessage: message, spaceId: spaceId)
-    newMessageView.translatesAutoresizingMaskIntoConstraints = false
-    contentView.addSubview(newMessageView)
-
-    let topConstraint: NSLayoutConstraint = if isThread, fromOtherSender {
-      newMessageView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 2)
-    } else {
-      newMessageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: fromOtherSender ? 12 : 2)
-    }
-
-    let leadingConstraint: NSLayoutConstraint = if isThread {
-      if outgoing {
-        newMessageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 32)
-      } else if fromOtherSender, let avatarView = avatarHostingController?.view {
-        newMessageView.leadingAnchor.constraint(equalTo: avatarView.trailingAnchor, constant: -2)
-      } else {
-        newMessageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 32)
-      }
-    } else {
-      newMessageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor)
-    }
-
-    let trailingConstraint: NSLayoutConstraint = if isThread {
-      if outgoing, let avatarView = avatarHostingController?.view {
-        newMessageView.trailingAnchor.constraint(equalTo: avatarView.leadingAnchor, constant: 2)
-      } else {
-        newMessageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -32)
-      }
-    } else {
-      newMessageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
-    }
-
-    NSLayoutConstraint.activate([
-      leadingConstraint,
-      trailingConstraint,
-      topConstraint,
-      newMessageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-    ])
-
-    messageView = newMessageView
-    contentView.transform = CGAffineTransform(scaleX: 1, y: -1)
   }
 
-  override func preferredLayoutAttributesFitting(
-    _ layoutAttributes: UICollectionViewLayoutAttributes
-  ) -> UICollectionViewLayoutAttributes {
-    let attributes = super.preferredLayoutAttributesFitting(layoutAttributes)
-    layoutIfNeeded()
-
-    let targetSize = CGSize(
-      width: layoutAttributes.frame.width,
-      height: UIView.layoutFittingCompressedSize.height
-    )
-
-    let size = contentView.systemLayoutSizeFitting(
-      targetSize,
-      withHorizontalFittingPriority: .required,
-      verticalFittingPriority: .fittingSizeLevel
-    )
-
-    attributes.frame.size = size
-    return attributes
+  func resetCell() {
+    messageView?.removeFromSuperview()
+    nameLabel.removeFromSuperview()
+    avatarHostingController?.view.removeFromSuperview()
+    avatarHostingController = nil
   }
 }
