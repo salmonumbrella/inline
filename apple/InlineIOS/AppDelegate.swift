9d4a82f1b7375a9a8b27b18861d0b92984806c9a
Dena Sohrabi <denasohrabi62@gmail.com>
2025-02-23 20:42:37 +0330

    Remove app icon badge

--- apple/InlineIOS/AppDelegate.swift
+++ apple/InlineIOS/AppDelegate.swift
@@ -48,7 +48,6 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
     Task {
       // Mark offline
       try? await DataManager.shared.updateStatus(online: false)
-      UnreadManager.shared.updateAppIconBadge()
     }
   }
 
@@ -56,7 +55,6 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
     Task {
       // Mark online
       try? await DataManager.shared.updateStatus(online: true)
-      UnreadManager.shared.updateAppIconBadge()
     }
   }
 
