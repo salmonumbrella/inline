38a239bb855337161bb7514dcc46390c66d54413
Dena Sohrabi <denasohrabi62@gmail.com>
2025-03-05 18:38:21 +0330

    Changes spaceItem UI

--- apple/InlineIOS/Main/RectangleSpaceItem.swift
+++ apple/InlineIOS/Main/RectangleSpaceItem.swift
@@ -14,18 +14,20 @@ struct RectangleSpaceItem: View {
       nav.push(.space(id: space.id))
     } label: {
       ZStack {
-        RoundedRectangle(cornerRadius: 18)
-          .strokeBorder(Color(.systemGray5), lineWidth: 1, antialiased: true)
-          .frame(width: 100, height: 100)
-          .overlay {
-            VStack {
-              SpaceAvatar(space: space, size: 32)
-              Text(space.name)
-                .font(.body)
-                .fontWeight(.medium)
-                .foregroundColor(.primary)
-            }
-          }
+        HStack {
+          SpaceAvatar(space: space, size: 28)
+          Text(space.name)
+            .font(.body)
+            .fontWeight(.medium)
+            .foregroundColor(.primary)
+        }
+        .frame(height: 48)
+        .padding(.horizontal, 14)
+        .background {
+          RoundedRectangle(cornerRadius: 25)
+//            .fill(Color(.systemGray6))
+            .strokeBorder(Color(.systemGray5), lineWidth: 1, antialiased: true)
+        }
       }
     }
   }
