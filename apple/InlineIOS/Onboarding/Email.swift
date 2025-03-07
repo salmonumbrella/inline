e1756d9dabadbc51ce1d77bbd8edbd86c8519fef
Dena Sohrabi <denasohrabi62@gmail.com>
2025-03-07 13:21:55 +0330

    Add email regex

--- apple/InlineIOS/Onboarding/Email.swift
+++ apple/InlineIOS/Onboarding/Email.swift
@@ -74,9 +74,12 @@ struct Email: View {
     }
   }
 
+  private let emailRegex = #"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#
+
   private func validateInput() {
     errorMsg = ""
-    isInputValid = !email.isEmpty && email.contains("@") && email.contains(".")
+    let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+    isInputValid = !email.isEmpty && emailPredicate.evaluate(with: email)
   }
 
   func submit() {
