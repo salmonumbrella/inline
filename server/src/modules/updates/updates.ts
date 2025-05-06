595bc2f6597999667f63eaf80518f66e1c33ac8a
Mohamad <mo.rajbi@gmail.com>
2025-05-07 00:07:23 +0330

    api: fix space updates by using userIDs directly

--- server/src/modules/updates/updates.ts
+++ server/src/modules/updates/updates.ts
@@ -7,19 +7,26 @@ export class Updates {
   static shared = new Updates()
 
   public async pushUpdate(updates: Update[], context: { peerId: InputPeer; currentUserId: number }) {
-    console.log("context", context)
     const updateGroup = await getUpdateGroupFromInputPeer(context.peerId, { currentUserId: context.currentUserId })
     switch (updateGroup.type) {
-      case "dmUsers":
+      case "dmUsers": {
         const { userIds } = updateGroup
         for (const userId of userIds) {
           RealtimeUpdates.pushToUser(userId, updates)
         }
         break
-      case "threadUsers":
-        const { spaceId } = updateGroup
-        RealtimeUpdates.pushToSpace(spaceId, updates)
+      }
+
+      case "threadUsers": {
+        const { userIds } = updateGroup
+        for (const userId of userIds) {
+          RealtimeUpdates.pushToUser(userId, updates)
+        }
+
+        // Unsure if connection manager is up to date on space members this can fail
+        // RealtimeUpdates.pushToSpace(spaceId, updates)
         break
+      }
     }
   }
 }
