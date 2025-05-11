63603cf9eec7ddd435316395361b1deb74a06a32
Mohamad <mo.rajbi@gmail.com>
2025-05-12 01:23:01 +0330

    migrate

---
+++ server/drizzle/0032_translation.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS "message_translations" (
+	"id" bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "message_translations_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 CACHE 1),
+	"date" timestamp (3) DEFAULT now() NOT NULL,
+	"message_id" bigint NOT NULL,
+	"chat_id" bigint NOT NULL,
+	"translation" "bytea",
+	"translation_iv" "bytea",
+	"translation_tag" "bytea",
+	"language" text NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "message_translations" ADD CONSTRAINT "message_translations_chat_id_chats_id_fk" FOREIGN KEY ("chat_id") REFERENCES "public"."chats"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "message_translations" ADD CONSTRAINT "message_id_chat_id_fk" FOREIGN KEY ("message_id","chat_id") REFERENCES "public"."messages"("chat_id","message_id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- server/src/db/schema/translations.ts
+++ server/src/db/schema/translations.ts
@@ -1,29 +1,38 @@
 import { bytea, creationDate } from "@in/server/db/schema/common"
 import { relations } from "drizzle-orm"
-import { pgTable, integer, text, bigint } from "drizzle-orm/pg-core"
+import { pgTable, integer, text, bigint, unique, foreignKey } from "drizzle-orm/pg-core"
 import { photos } from "./media"
 import { messages } from "@in/server/db/schema/messages"
 import { chats } from "@in/server/db/schema/chats"
 
-export const translations = pgTable("message_translations", {
-  id: bigint("id", { mode: "number" }).generatedAlwaysAsIdentity().primaryKey(),
-  date: creationDate,
+export const translations = pgTable(
+  "message_translations",
+  {
+    id: bigint("id", { mode: "number" }).generatedAlwaysAsIdentity().primaryKey(),
+    date: creationDate,
 
-  messageId: bigint("message_id", { mode: "number" })
-    .references(() => messages.messageId)
-    .notNull(),
-  chatId: bigint("chat_id", { mode: "number" })
-    .references(() => chats.id)
-    .notNull(),
+    messageId: bigint("message_id", { mode: "number" }).notNull(),
+    chatId: bigint("chat_id", { mode: "number" })
+      .references(() => chats.id)
+      .notNull(),
 
-  // Encrypted translation text
-  translation: bytea("translation"),
-  translationIv: bytea("translation_iv"),
-  translationTag: bytea("translation_tag"),
+    // Encrypted translation text
+    translation: bytea("translation"),
+    translationIv: bytea("translation_iv"),
+    translationTag: bytea("translation_tag"),
 
-  // Language code (e.g. "en", "es", "fr")
-  language: text("language").notNull(),
-})
+    // Language code (e.g. "en", "es", "fr")
+    language: text("language").notNull(),
+  },
+  (t) => ({
+    /** Ensure messageId and chatId are valid */
+    messageIdChatIdForeignKey: foreignKey({
+      name: "message_id_chat_id_fk",
+      columns: [t.messageId, t.chatId],
+      foreignColumns: [messages.chatId, messages.messageId],
+    }),
+  }),
+)
 
 export const translationsRelations = relations(translations, ({ one }) => ({
   message: one(messages, {
