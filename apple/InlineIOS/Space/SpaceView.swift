cf1962ee6b5990bddbba173de86bab790219ce86
Mohamad <mo.rajbi@gmail.com>
2025-05-13 01:02:44 +0330

    trigger build

--- apple/InlineIOS/Space/SpaceView.swift
+++ apple/InlineIOS/Space/SpaceView.swift
@@ -37,7 +37,6 @@ struct SpaceView: View {
 
   @EnvironmentStateObject var fullSpaceViewModel: FullSpaceViewModel
 
-  // trigger build
   @State private var navBarHeight: CGFloat = 0
   @State private var selectedTab: Tabs = .chats
 
