8f8125b24f7ded29297382d67f2fa0b7f320b83b
Dena Sohrabi <87666169+dena-sohrabi@users.noreply.github.com>
2025-05-27 01:58:04 +0330

    Revert "Fix"
    This reverts commit 82efe3ff0ba8fe070d86f1223010738627852200.

--- server/src/methods/notion/createNotionTask.ts
+++ server/src/methods/notion/createNotionTask.ts
@@ -47,13 +47,10 @@ export const handler = async (
         chatId,
         currentUserId: context.currentUserId,
       }),
-
-      // message exists check
       db
         .select({ count: count() })
         .from(messages)
-        .where(eq(messages.globalId, BigInt(messageId)))
-        // .where(and(eq(messages.globalId, BigInt(messageId)), eq(messages.chatId, chatId)))
+        .where(and(eq(messages.messageId, messageId), eq(messages.chatId, chatId)))
         .then((result) => result[0]!.count > 0),
     ])
 
