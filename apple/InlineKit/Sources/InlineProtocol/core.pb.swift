f81e4e6528cc4ceae2a0bfae89b8c2efc99d2a03
Dena Sohrabi <87666169+dena-sohrabi@users.noreply.github.com>
2025-06-15 20:47:30 +0330

    proto: add draft message

--- apple/InlineKit/Sources/InlineProtocol/core.pb.swift
+++ apple/InlineKit/Sources/InlineProtocol/core.pb.swift
@@ -4348,6 +4348,30 @@ public struct RemoveChatParticipantResult: Sendable {
   public init() {}
 }
 
+/// Apple only types
+public struct DraftMessage: Sendable {
+  // SwiftProtobuf.Message conformance is added in an extension below. See the
+  // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
+  // methods supported on all messages.
+
+  public var text: String = String()
+
+  public var entities: MessageEntities {
+    get {return _entities ?? MessageEntities()}
+    set {_entities = newValue}
+  }
+  /// Returns true if `entities` has been explicitly set.
+  public var hasEntities: Bool {return self._entities != nil}
+  /// Clears the value of `entities`. Subsequent reads from it will return its default value.
+  public mutating func clearEntities() {self._entities = nil}
+
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
+
+  public init() {}
+
+  fileprivate var _entities: MessageEntities? = nil
+}
+
 // MARK: - Code below here is support for the SwiftProtobuf runtime.
 
 extension Method: SwiftProtobuf._ProtoNameProviding {
@@ -11205,6 +11229,48 @@ extension RemoveChatParticipantResult: SwiftProtobuf.Message, SwiftProtobuf._Mes
   }
 
   public static func ==(lhs: RemoveChatParticipantResult, rhs: RemoveChatParticipantResult) -> Bool {
+    if lhs.unknownFields != rhs.unknownFields {return false}
+    return true
+  }
+}
+
+extension DraftMessage: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
+  public static let protoMessageName: String = "DraftMessage"
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+    1: .same(proto: "text"),
+    2: .same(proto: "entities"),
+  ]
+
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+    while let fieldNumber = try decoder.nextFieldNumber() {
+      // The use of inline closures is to circumvent an issue where the compiler
+      // allocates stack space for every case branch when no optimizations are
+      // enabled. https://github.com/apple/swift-protobuf/issues/1034
+      switch fieldNumber {
+      case 1: try { try decoder.decodeSingularStringField(value: &self.text) }()
+      case 2: try { try decoder.decodeSingularMessageField(value: &self._entities) }()
+      default: break
+      }
+    }
+  }
+
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+    // The use of inline closures is to circumvent an issue where the compiler
+    // allocates stack space for every if/case branch local when no optimizations
+    // are enabled. https://github.com/apple/swift-protobuf/issues/1034 and
+    // https://github.com/apple/swift-protobuf/issues/1182
+    if !self.text.isEmpty {
+      try visitor.visitSingularStringField(value: self.text, fieldNumber: 1)
+    }
+    try { if let v = self._entities {
+      try visitor.visitSingularMessageField(value: v, fieldNumber: 2)
+    } }()
+    try unknownFields.traverse(visitor: &visitor)
+  }
+
+  public static func ==(lhs: DraftMessage, rhs: DraftMessage) -> Bool {
+    if lhs.text != rhs.text {return false}
+    if lhs._entities != rhs._entities {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
   }
--- server/packages/protocol/src/core.ts
+++ server/packages/protocol/src/core.ts
@@ -2877,6 +2877,21 @@ export interface RemoveChatParticipantInput {
 export interface RemoveChatParticipantResult {
 }
 /**
+ * Apple only types
+ *
+ * @generated from protobuf message DraftMessage
+ */
+export interface DraftMessage {
+    /**
+     * @generated from protobuf field: string text = 1;
+     */
+    text: string;
+    /**
+     * @generated from protobuf field: optional MessageEntities entities = 2;
+     */
+    entities?: MessageEntities;
+}
+/**
  * @generated from protobuf enum Method
  */
 export enum Method {
@@ -10352,3 +10367,57 @@ class RemoveChatParticipantResult$Type extends MessageType<RemoveChatParticipant
  * @generated MessageType for protobuf message RemoveChatParticipantResult
  */
 export const RemoveChatParticipantResult = new RemoveChatParticipantResult$Type();
+// @generated message type with reflection information, may provide speed optimized methods
+class DraftMessage$Type extends MessageType<DraftMessage> {
+    constructor() {
+        super("DraftMessage", [
+            { no: 1, name: "text", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
+            { no: 2, name: "entities", kind: "message", T: () => MessageEntities }
+        ]);
+    }
+    create(value?: PartialMessage<DraftMessage>): DraftMessage {
+        const message = globalThis.Object.create((this.messagePrototype!));
+        message.text = "";
+        if (value !== undefined)
+            reflectionMergePartial<DraftMessage>(this, message, value);
+        return message;
+    }
+    internalBinaryRead(reader: IBinaryReader, length: number, options: BinaryReadOptions, target?: DraftMessage): DraftMessage {
+        let message = target ?? this.create(), end = reader.pos + length;
+        while (reader.pos < end) {
+            let [fieldNo, wireType] = reader.tag();
+            switch (fieldNo) {
+                case /* string text */ 1:
+                    message.text = reader.string();
+                    break;
+                case /* optional MessageEntities entities */ 2:
+                    message.entities = MessageEntities.internalBinaryRead(reader, reader.uint32(), options, message.entities);
+                    break;
+                default:
+                    let u = options.readUnknownField;
+                    if (u === "throw")
+                        throw new globalThis.Error(`Unknown field ${fieldNo} (wire type ${wireType}) for ${this.typeName}`);
+                    let d = reader.skip(wireType);
+                    if (u !== false)
+                        (u === true ? UnknownFieldHandler.onRead : u)(this.typeName, message, fieldNo, wireType, d);
+            }
+        }
+        return message;
+    }
+    internalBinaryWrite(message: DraftMessage, writer: IBinaryWriter, options: BinaryWriteOptions): IBinaryWriter {
+        /* string text = 1; */
+        if (message.text !== "")
+            writer.tag(1, WireType.LengthDelimited).string(message.text);
+        /* optional MessageEntities entities = 2; */
+        if (message.entities)
+            MessageEntities.internalBinaryWrite(message.entities, writer.tag(2, WireType.LengthDelimited).fork(), options).join();
+        let u = options.writeUnknownFields;
+        if (u !== false)
+            (u == true ? UnknownFieldHandler.onWrite : u)(this.typeName, message, writer);
+        return writer;
+    }
+}
+/**
+ * @generated MessageType for protobuf message DraftMessage
+ */
+export const DraftMessage = new DraftMessage$Type();
